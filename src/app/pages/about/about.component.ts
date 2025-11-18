9ad26efc857a0151b9fc4bd27adf2523a696ef84
JULIO CESAR REYES ALMARAZ <julio.reyes@funcionpublica.gob.mx>
2025-11-18 12:34:01 -0600

    Add video

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,14 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Pipe } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
+import { DomSanitizer } from '@angular/platform-browser';
+import { UrlSafePipe } from '../../pipes/url-safe-pipe';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [MatListModule, MatCardModule],
+  imports: [MatListModule, MatCardModule, UrlSafePipe],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss',
 })
 export class AboutComponent {
-
+  
+  videoList = [
+    'https://www.youtube.com/embed/735r_ho18EE'
+  ];
 }
